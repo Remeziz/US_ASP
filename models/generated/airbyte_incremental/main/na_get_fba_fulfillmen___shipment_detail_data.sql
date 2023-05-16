d5d04513f03c154cfccde772ea4bca7cd4c082ff
Remeziz <79668248+Remeziz@users.noreply.github.com>
2023-05-16 11:41:35 +0300

    Update and rename us_get_fba_fulfillmen___shipment_detail_data.sql to na_get_fba_fulfillmen___shipment_detail_data.sql

--- models/generated/airbyte_incremental/main/na_get_fba_fulfillmen___shipment_detail_data.sql
+++ models/generated/airbyte_incremental/main/na_get_fba_fulfillmen___shipment_detail_data.sql
@@ -4,7 +4,7 @@
     tags = [ "top-level" ]
 ) }}
 -- Final base SQL model
--- depends_on: {{ ref('us_get_fba_fulfillmen_1pment_detail_data_ab3') }}
+-- depends_on: {{ ref('na_get_fba_fulfillmen_1pment_detail_data_ab3') }}
 select
     carrier,
     disposition,
@@ -18,9 +18,9 @@ select
     _airbyte_ab_id,
     _airbyte_emitted_at,
     {{ current_timestamp() }} as _airbyte_normalized_at,
-    _airbyte_us_get_fba_f__nt_detail_data_hashid
-from {{ ref('us_get_fba_fulfillmen_1pment_detail_data_ab3') }}
--- us_get_fba_fulfillmen___shipment_detail_data from {{ source('main', '_airbyte_raw_us_get_f___shipment_detail_data') }}
+    _airbyte_na_get_fba_f__nt_detail_data_hashid
+from {{ ref('na_get_fba_fulfillmen_1pment_detail_data_ab3') }}
+-- na_get_fba_fulfillmen___shipment_detail_data from {{ source('main', '_airbyte_raw_na_get_f___shipment_detail_data') }}
 where 1 = 1
 {{ incremental_clause('_airbyte_emitted_at', this) }}
 
