6429f5044d7d481c32fa23b7eb74a5d5d7f20279
Remeziz <79668248+Remeziz@users.noreply.github.com>
2023-05-16 11:38:04 +0300

    Update and rename us_get_fba_fulfillmen_1_replacement_data_ab3.sql to na_get_fba_fulfillmen_1_replacement_data_ab3.sql

--- models/generated/airbyte_ctes/main/na_get_fba_fulfillmen_1_replacement_data_ab3.sql
+++ models/generated/airbyte_ctes/main/na_get_fba_fulfillmen_1_replacement_data_ab3.sql
@@ -4,7 +4,7 @@
     tags = [ "top-level-intermediate" ]
 ) }}
 -- SQL model to build a hash column based on the values of this record
--- depends_on: {{ ref('us_get_fba_fulfillmen_1_replacement_data_ab2') }}
+-- depends_on: {{ ref('na_get_fba_fulfillmen_1_replacement_data_ab2') }}
 select
     {{ dbt_utils.surrogate_key([
         'quantity',
@@ -16,10 +16,10 @@ select
         'asin',
         'sku',
         adapter.quote('original-fulfillment-center-id'),
-    ]) }} as _airbyte_us_get_fba_f__placement_data_hashid,
+    ]) }} as _airbyte_na_get_fba_f__placement_data_hashid,
     tmp.*
-from {{ ref('us_get_fba_fulfillmen_1_replacement_data_ab2') }} tmp
--- us_get_fba_fulfillmen__ment_replacement_data
+from {{ ref('na_get_fba_fulfillmen_1_replacement_data_ab2') }} tmp
+-- na_get_fba_fulfillmen__ment_replacement_data
 where 1 = 1
 {{ incremental_clause('_airbyte_emitted_at', this) }}
 
